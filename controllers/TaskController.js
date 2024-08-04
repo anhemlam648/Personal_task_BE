b31563ce67d95e2b7447edb70b2e349ba3a2565f
VuTrungNghia <vunghia467@gmail.com>
2024-08-04 15:18:43 +0700

    createControllerTask

---
+++ controllers/TaskController.js
@@ -0,0 +1,17 @@
+const Task = require('../models/Task');
+
+exports.createTask = async (req, res) => {
+
+};
+
+exports.getTasks = async (req, res) => {
+ 
+};
+
+exports.updateTask = async (req, res) => {
+ 
+};
+
+exports.deleteTask = async (req, res) => {
+ 
+};
