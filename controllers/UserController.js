e5c0ad94ebeda5bf6698694f9b6010e0339daa65
VuTrungNghia <vunghia467@gmail.com>
2024-08-04 15:17:31 +0700

    createControllerUser

---
+++ controllers/UserController.js
@@ -0,0 +1,10 @@
+const admin = require('../config/firebase');
+const User = require('../models/User');
+
+exports.registerUser = async (req, res) => {
+
+};
+
+exports.loginUser = async (req, res) => {
+  
+};
