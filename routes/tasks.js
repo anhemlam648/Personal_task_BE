3f07beacd20ee30809af6671896854ba647d6a43
VuTrungNghia <vunghia467@gmail.com>
2024-08-06 11:48:58 +0700

    RoutesTask and Finish

--- routes/tasks.js
+++ routes/tasks.js
@@ -3,9 +3,10 @@ const router = express.Router();
 const taskController = require('../controllers/TaskController');
 const firebaseUser = require('../middlewares/firebaseUserMiddleware');
 
-router.post('/addtask', firebaseUser, taskController.createTask);
-router.get('/', firebaseUser, taskController.getTasks);
-router.put('/edit/:taskId', firebaseUser, taskController.updateTask);
-router.delete('/delete/:taskId',firebaseUser, taskController.deleteTask);
+router.post('/addtask', firebaseUser, taskController.createTask); // add Task
+router.get('/', firebaseUser, taskController.getTasks); // get all Task
+router.get('/tasks/:taskId', firebaseUser, taskController.getTaskById); //get Task by Id
+router.put('/edit/:taskId', firebaseUser, taskController.updateTask); // edit Task by Id
+router.delete('/delete/:taskId',firebaseUser, taskController.deleteTask); // delete Task by Id
 
 module.exports = router;
